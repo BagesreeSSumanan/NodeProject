18f4ca445f80f820c071eec86557244de410c482
Bagesree S Sumanan <bagesree.s.sumanan@xminds.com>
2025-09-26 18:07:02 +0530

    Node.JS project

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports={
+    port:process.env.PORT,
+    database:{
+        dbport:process.env.DB_PORT,
+        dbhost:process.env.HOST,
+        dbuser:process.env.DB_USER,
+        dbpassword:process.env.DB_PASSWORD,
+        database :process.env.DATABASE,
+        dbdialect:process.env.dialect
+    }
+}
---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const config = require('./config');
+const empRouter = express.Router();
+const app = express();
+require('dotenv').config();
+const   insertEmployee= require('./empController');
+// const port = 3000;
+app.use(express.json());
+const PORT = process.env.PORT
+console.log(PORT);
+// app.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});
+
+// app.post('/', (req, res) => {
+
+//   res.send('Got a POST request')
+
+// })
+
+
+// app.put('/user', (req, res) => {
+
+//   res.send('Got a PUT request at /user')
+
+// })
+// app.delete('/user', (req, res) => {
+
+//   res.send('Got a DELETE request at /user')
+
+// })
+
+
+// app.post('/api/users', (req, res) => {
+//   const { name, email } = req.body;
+
+//   const newUser = {
+//     id: Date.now(),
+//     name,
+//     email
+//   };
+//   res.status(201).json({
+//     message: 'User created successfully',
+//     user: newUser
+//   });
+// })
+
+// let users = [
+//   { id: 1, name: "Bagesree", email: "bagesree@xminds.com" },
+//   { id: 2, name: "Hari", email: "hari@gmail.com" }
+// ];
+// app.put('/api/userUpdate',(req,res)=>{
+//     const {id,name, email} = req.body;
+
+//     let user = users.find(u => u.id === parseInt(id));
+//     user.name = name;
+//     user.email = email;
+
+//     res.status(201).json({
+//         message:'User updated successfully',
+//         user : user
+//     })
+// })
+
+// Create an employee
+  empRouter.post('/',  async (req, res, next)=>{
+    console.log("employee post");
+     try{
+         const name = req.body.employee.name;
+         const designation = req.body.employee.designation;
+         const email = req.body.employee.email;
+         const age = req.body.employee.age;
+               if (!name || !designation || !age) {
+                 return res.sendStatus(400);
+              }
+         const employee =  await insertEmployee(name, email,designation,age).then(() => res.json({ message: 'Employee created.' }));
+
+     } catch(e){
+         console.log(e);
+         res.sendStatus(400);
+     }
+  });
+
+// app.get('/', (req, res) => {
+//   res.send('Welcome to Employee API');
+// });
